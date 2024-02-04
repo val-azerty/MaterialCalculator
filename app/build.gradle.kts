07fbbc9df73ff1cc70a26faa57adacdd7d7801ad
Valentin <valentin.delaunay2@gmail.com>
2024-02-04 20:03:17 +0100

    Montée de version de java à 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.materialcalculator"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.materialcalculator"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
